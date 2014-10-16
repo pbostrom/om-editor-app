fd26b5d5b8ea752bf3db76629b3f09dfc66b18d4
Paul Bostrom <pbostrom@roomkey.com>
2014-10-16 10:14:53 -0400

    Update README and deps

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# om-editor
+# om-editor-app
 
-A Clojure library designed to ... well, that part is up to you.
+An example app using the om-codemirror library, an Om wrapper around the CodeMirror JavaScript text editor. See https://github.com/pbostrom/om-codemirror for more info.
 
 ## Usage
+Add the `lein-cljsbuild` and `lein-cljsasset` plugins to `project.clj`.
+```clj
+:plugins [[lein-cljsbuild "1.0.3"]
+          [lein-cljsasset "0.1.0"]]
+```
+Run the plugins:
+    $ lein cljsbuild once
+    $ lein cljsasset
 
-FIXME
+then load `index.html` in your browser.
 
+The editor is configured with two hotkeys: Ctrl-Shift-Z and Ctrl-Shift-X. Currently they just print to the console.
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 Paul Bostrom
+Licensed under the Eclipse Public License.
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+http://www.eclipse.org/legal/epl-v10.html
+
--- project.clj
+++ project.clj
@@ -1,24 +1,22 @@
-(defproject om-editor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject om-editor-app "0.1.0-SNAPSHOT"
+  :description "Example app using om-codemirror"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2227"]
-                 [om-codemirror "0.2.0-SNAPSHOT"]
-                 [om "0.6.5-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [om-codemirror "0.2.0"]
+                 [pbostrom/om "0.7.3-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-cljsasset "0.1.0-SNAPSHOT"]]
+            [lein-cljsasset "0.1.0"]]
 
-  :cljsasset {:js []
-              :css []
-              :js-output {:dir "resources/js"
-                          :file "assets.js"}
-              :css-output {:dir "resources/css"
-                           :file "assets.css"}}
+  ;; :cljsasset {:js-output {:dir "resources/public/js"
+  ;;                         :file "assets.js"}
+  ;;             :css-output {:dir "resources/public/css"
+  ;;                          :file "assets.css"}}
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:source-paths ["src"]
               :compiler {
                 :output-to "app.js"
