cb96affbcc676ba59846ac83dd04bdca24aaf51f
Paul Bostrom <pbostrom@roomkey.com>
2014-10-17 10:07:53 -0400

    Bump deps

--- project.clj
+++ project.clj
@@ -5,16 +5,19 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2322"]
-                 [om-codemirror "0.2.0"]
-                 [pbostrom/om "0.7.3"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [om-codemirror "0.2.1"]
+                 [om "0.7.3"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-cljsasset "0.1.0"]]
+            [lein-cljsasset "0.2.0"]]
 
-  ;; :cljsasset {:js-output {:dir "resources/public/js"
-  ;;                         :file "assets.js"}
-  ;;             :css-output {:dir "resources/public/css"
-  ;;                          :file "assets.css"}}
+  :cljsasset {:js ["react/react.js"]
+              ;; :js-output {:dir "resources/public/js"
+              ;;             :file "assets.js"}
+              ;; :css-output {:dir "resources/public/css"
+              ;;              :file "assets.css"}
+              }
 
   :cljsbuild {
     :builds [{:source-paths ["src"]
